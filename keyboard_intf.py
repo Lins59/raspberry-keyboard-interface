285bc5bae9401328f2d0507d538675bbafa7e6e1
[a551894] Pierre Quételart <pierre.quetelart@atos.net>
2017-02-09 09:55:02 +0100

    Add keymap

--- keyboard_intf.py
+++ keyboard_intf.py
@@ -10,29 +10,29 @@ GPIO.setmode(GPIO.BCM)
 # | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
 # +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
 # |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
-# |   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5V      |     |     |
-# |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
-# |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 0 | IN   | TxD     | 15  | 14  |
-# |     |     |      0v |      |   |  9 || 10 | 1 | IN   | RxD     | 16  | 15  |
-# |  17 |   0 | GPIO. 0 |   IN | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |
-# |  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
-# |  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
-# |     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
-# |  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
-# |   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
-# |  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
-# |     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
+# |   2 |   8 |   SDA.1 |  OUT | 0 |  3 || 4  |   |      | 5V      |     |     |
+# |   3 |   9 |   SCL.1 |  OUT | 0 |  5 || 6  |   |      | 0v      |     |     |
+# |   4 |   7 | GPIO. 7 |  OUT | 0 |  7 || 8  | 0 | PU   | TxD     | 15  | 14  |
+# |     |     |      0v |      |   |  9 || 10 | 0 | PU   | RxD     | 16  | 15  |
+# |  17 |   0 | GPIO. 0 |  OUT | 0 | 11 || 12 | 0 | PU   | GPIO. 1 | 1   | 18  |
+# |  27 |   2 | GPIO. 2 |  OUT | 0 | 13 || 14 |   |      | 0v      |     |     |
+# |  22 |   3 | GPIO. 3 |   PU | 0 | 15 || 16 | 0 | PU   | GPIO. 4 | 4   | 23  |
+# |     |     |    3.3v |      |   | 17 || 18 | 0 | PU   | GPIO. 5 | 5   | 24  |
+# |  10 |  12 |    MOSI |   PU | 0 | 19 || 20 |   |      | 0v      |     |     |
+# |   9 |  13 |    MISO |   PU | 0 | 21 || 22 | 0 | PU   | GPIO. 6 | 6   | 25  |
+# |  11 |  14 |    SCLK |   PU | 0 | 23 || 24 | 0 | PU   | CE0     | 10  | 8   |
+# |     |     |      0v |      |   | 25 || 26 | 0 | PU   | CE1     | 11  | 7   |
 # |     |     |         |      |   |    ||    |   |      |         |     |     |
 # |     |     |         |      |   |  BELOW   |   |      |         |     |     |
 # |     |     | USED BY JARVIS PROGRAM TO CONTROL RELAYS |         |     |     |
 # |     |     |         |      |   |    ||    |   |      |         |     |     |
-# |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
-# |   5 |  21 | GPIO.21 |   IN | 0 | 29 || 30 |   |      | 0v      |     |     |
-# |   6 |  22 | GPIO.22 |   IN | 0 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
-# |  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
-# |  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
-# |  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
-# |     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
+# |   0 |  30 |   SDA.0 | RELAY| 1 | 27 || 28 | 1 | RELAY| SCL.0   | 31  | 1   |
+# |   5 |  21 | GPIO.21 | RELAY| 0 | 29 || 30 |   |      | 0v      |     |     |
+# |   6 |  22 | GPIO.22 | RELAY| 0 | 31 || 32 | 0 | RELAY| GPIO.26 | 26  | 12  |
+# |  13 |  23 | GPIO.23 | RELAY| 0 | 33 || 34 |   |      | 0v      |     |     |
+# |  19 |  24 | GPIO.24 | RELAY| 0 | 35 || 36 | 0 | RELAY| GPIO.27 | 27  | 16  |
+# |  26 |  25 | GPIO.25 | RELAY| 0 | 37 || 38 | 0 | RELAY| GPIO.28 | 28  | 20  |
+# |     |     |      0v |      |   | 39 || 40 | 0 | RELAY| GPIO.29 | 29  | 21  |
 # +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
 # | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
 # +-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
@@ -40,16 +40,58 @@ GPIO.setmode(GPIO.BCM)
 #  5 rows
 rows = [2,3,4,17,27]
 
-# 10 columns
-columns = [22,10,14,15,18,23,24,25,8,7]
+# 12 columns
+columns = [22,10,9,11,14,15,18,23,24,25,8,7]
 
 # list keys
-keys = {}
-keys[2] = {22:'1', 10:'2', 14:'3', 15:'4', 18:'5', 23:'6', 24:'7', 25:'8', 8:'9', 7:'0'}
-keys[3] = {22:'a', 10:'z', 14:'e', 15:'r', 18:'t', 23:'y', 24:'u', 25:'i', 8:'o', 7:'p'}
-keys[4] = {22:'q', 10:'s', 14:'d', 15:'f', 18:'g', 23:'h', 24:'j', 25:'k', 8:'l', 7:'m'}
-keys[17] = {22:'w', 10:'x', 14:'c', 15:'v', 18:'b', 23:'n', 24:',', 25:';', 8:':', 7:'!'}
-keys[27] = {22:'a', 10:'z', 14:'e', 15:'r', 18:'t', 23:'y', 24:'u', 25:'i', 8:'o', 7:'p'}
+keymap = []
+keymap.append("$|1 é|2 \"|3 '|4 (|5 -|6 è|7 _|8 ç|9 à|& =|+ )|°")
+keymap.append("a z e r t y u i o p ACCENT")
+keymap.append("q s d f g h j k l m ù|%")
+keymap.append("w x c v b n ,|? ;|. :|/ !|§")
+keymap.append("SHIFT SPACE CAPS BACK_SPACE")
+
+if keymap.length != rows.length:
+  raise Exception('Keymap length differs from rows length')
+
+keys = []
+for index, row in enumerate(rows):
+  current_keymap_row = keymap[index]
+  
+  # Split on space
+  split_chars = current_keymap_row.split(' ')
+  
+  if split_chars.length > columns.length:
+    raise Exception('Keymap row is greater than column length')
+  
+  for indexColumn, column in enumerate(columns):
+    current_chars = split_chars[indexColumn]
+    lower_char, upper_char
+    if '|' in current_chars:
+      # First char = "lower" case
+      # Second char = "upper" case
+      (lower_char, upper_char) = current_chars.split('|')
+    elif len(current_chars) == 0:
+      lower_char = current_chars
+      upper_char = current_chars.upper()
+    # Specials chars
+    elif current_chars == "ACCENT":
+      lower_char = '^'
+      upper_char = '¨'
+    elif current_chars == "SHIFT":
+      lower_char = 'shift'
+      upper_char = 'shift'
+    elif current_chars == "CAPS":
+      lower_char = 'caps lock'
+      upper_char = 'caps lock'
+    elif current_chars == "BACK_SPACE":
+      lower_char = 'backspace'
+      upper_char = 'backspace'
+    
+    keys[row][column]['lower'] = lower_char
+    keys[row][column]['upper'] = upper_char
+
+print keys
 
 current_row = 0
 
@@ -60,7 +102,7 @@ def trigger_input(column):
     keyboard.write(keys[current_row][column])
 
 # INIT
-# 5 rows x 10 columns = 50 keys matrix
+# 5 rows x 12 columns = 60 keys matrix
 
 # Columns are output
 for row in rows:
